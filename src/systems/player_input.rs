4b9407a8ca989172937aef7bf532780cfd6dae0d
Leo Park <61398869+dg-leo-park@users.noreply.github.com>
2023-04-17 11:54:31 +0900

    apply turn system

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -7,7 +7,8 @@ pub fn player_input(
     ecs: &mut SubWorld,
     #[resource] map: &Map,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera
+    #[resource] camera: &mut Camera,
+    #[resource] turn_state: &mut TurnState
 ) {
     if let Some(key) = key {
         let delta: Point = match key {
@@ -26,6 +27,7 @@ pub fn player_input(
                 if map.can_enter_tile(dest) {
                     *pos = dest;
                     camera.on_player_move(dest);
+                    *turn_state = TurnState::PlayerTurn;
                 }
             });
         }
